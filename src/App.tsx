400dbc62abe82e1b3e1529044139f872573a1fba
huydinh-uit <19520589@gm.uit.edu.vn>
2023-05-08 00:37:46 +0700

    Homepage,Navbar,Footer,SearchBookPage fetched

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+//import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+// import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+// import { oktaConfig } from './lib/oktaConfig';
+// import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+// import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+// import LoginWidget from './Auth/LoginWidget';
+// import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+// import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
+// import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
+// import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
-function App() {
+export const App = ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <Navbar/>
+        <HomePage/>
+        <Footer/>
+      </div>
+  )
 }
-
-export default App;
